e84fcb6e04282f3b7d84b96e8fe79b59fded2a52
Jordan Christiansen <xordspar0@gmail.com>
2016-11-03 14:42:21 -0500

    Added first working version of the graph generation script

--- generateGraph.sh
+++ generateGraph.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
 APIKEY=$(cat APIKEY)
-curl "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=${APIKEY}"
+
+oldIFS=$IFS
+cities=($(cat cities.txt | sed 's/ /%20/g'))
+
+echo "" > graph.txt
+for i in $(seq 0 3)
+do
+	origin=${cities[$i]}
+	for j in $(seq $(($i + 1)) 49)
+	do
+		destination=${cities[$j]}
+		miles=$(curl -s "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin}&destinations=${destination}&key=${APIKEY}" | grep -Eo '"[0-9,]+ mi"' | grep -Eo '[0-9,]+' | sed 's/,//g')
+		echo $i $j $miles >> graph.txt
+	done
+done
 
