1f62ffdf4b46974b249662f41969f9ac373ea35e
Frank19 <jzijziba@gmail.com>
2016-11-29 15:11:17 -0600

    added a few ideas (WIP)

--- Selection.cpp
+++ Selection.cpp
@@ -1,1 +1,48 @@
 // Franklin Slaby
+
+//Fitness Determination
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main()
+{
+	//vector of routes
+	routeList[GEN_SIZE][2]
+	
+	//calculate 20% of the vector
+	int topSelection = GEN_SIZE/5;
+
+	// calculate 5% of the vector	
+	int randomSelection = GEN_SIZE/20;
+
+	//Look at a route
+	for (int i = 0, i < GEN_SIZE, i++)
+		{
+	
+			routes[i].weight
+	
+		}
+	
+	for (int j = 0, j < topSelection, j++)
+		{
+			//find best scores (smallest weights)
+		
+			//add route to vector or array of top 
+		
+			//remove route from list of route to be examined
+		
+		}
+	
+		for (int k = 0, k < randomSelection, k++)
+		{
+			//randomly select a number
+			
+			//
+			
+			
+			
+		}
+}
