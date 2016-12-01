ea6d588b4e038e0b639c2ed2460e1c9e50c99978
Frank19 <jzijziba@gmail.com>
2016-12-01 02:33:08 -0600

    updated a little (WIP)

--- Selection.cpp
+++ Selection.cpp
@@ -4,14 +4,18 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <fstream>
 #include <vector>
 
 int main()
 {
 	//vector of routes
-	routeList[GEN_SIZE][2]
+	std::vector<pair<double, double>> tempRoute;
+	std::vector<pair<double, double>> selectedRoutes;
+	std::vector<pair<double, double>> minVector;
 	
+	//minimum variable
+	double minWeight = 0;
+		
 	//calculate 20% of the vector
 	int topSelection = GEN_SIZE/5;
 
@@ -21,19 +25,32 @@ int main()
 	//Look at a route
 	for (int i = 0, i < GEN_SIZE, i++)
 		{
-	
-			routes[i].weight
+			//add route to temporary list
+			tempRoute.push_back(make_pair(routes[i].cityList, routes[i].weight));
 	
 		}
-	
+	//grab 20% of routes
 	for (int j = 0, j < topSelection, j++)
 		{
-			//find best scores (smallest weights)
-		
+			//sort through to find best scores
+			for (int e = 0, e < GEN_SIZE, e++)
+			{
+				//find best scores (smallest weights)
+				if (minWeight == 0 || minweight > tempRoute[j].second)
+				{				
+					minVector.clear();
+					minWeight = tempRoute[j].second;
+					minVector.push_back(make_pair(tempRoute[j].first, tempRoute[j].second));
+				}
+			}
 			//add route to vector or array of top 
-		
+			selectedRoutes.push_back(make_pair(minVector[j].first, minVector[j].second));
+			
 			//remove route from list of route to be examined
 		
+		
+			//reset minimum
+			minWeight = 0;
 		}
 	
 		for (int k = 0, k < randomSelection, k++)
