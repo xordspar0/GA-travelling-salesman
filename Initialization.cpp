9a9d58b59efa9d9e7aa9b55e121c2d135b6f317c
Jordan Christiansen <xordspar0@gmail.com>
2016-12-01 14:20:52 -0600

    Added a route generation function that does not require empty vectors

--- Initialization.cpp
+++ Initialization.cpp
@@ -19,6 +19,12 @@ class Route
 		double weight;
 };
 
+Route generate(vector< vector<double> > aWE, int e, int n) {
+	vector<double> emptyVector;
+	Route emptyRoute;
+
+	return generate(aWE, e, n, emptyVector, emptyRoute);
+}
 /*
 Function to generate a new route or finish a preexisting route
 	aWE is the vector containing all of the edges and weights from the file
@@ -29,30 +35,30 @@ Function to generate a new route or finish a preexisting route
 */
 Route generate(vector< vector<double> > aWE, int e, int n, Route pR, vector<double> cV)
 {
-	
+
 	double currentCity, totalWeight;
-	
+
 	int currentEdge, ccIndex, noVisited, currentCityEdges;
-	
+
 	vector<double> citiesVisited;
 	vector<double> nList;
-	
+
 	Route outputRoute;
-	
+
 	/*
-	if a partially complete route is passed to the function, 
+	if a partially complete route is passed to the function,
 	citiesVisited is set equal to that vector, noVisited is
-	set to the size of that vector, and totalWeight is set 
+	set to the size of that vector, and totalWeight is set
 	to the total weight of that route
 	*/
 	citiesVisited = cV;
 	noVisited = citiesVisited.size();
 	totalWeight = pR.weight;
 	nList.swap(pR.nodeList);
-	
+
 	//initial edge randomly selected here
 	currentEdge = rand()%675;
-	
+
 	//first two cities are added to citiesVisited array here
 	citiesVisited.push_back(aWE[currentEdge][0]);
 	citiesVisited.push_back(aWE[currentEdge][1]);
@@ -60,10 +66,10 @@ Route generate(vector< vector<double> > aWE, int e, int n, Route pR, vector<doub
 	nList.push_back(aWE[currentEdge][1]);
 	currentCity = aWE[currentEdge][1];
 	totalWeight += aWE[currentEdge][2];
-	
+
 	/*
 	the condition for this loop should be (noVisited < n), where n is the number of nodes
-	however, the program constantly hits the totalWeight hard cap when that is the case. 
+	however, the program constantly hits the totalWeight hard cap when that is the case.
 	*/
 	while(noVisited < 258)
 	{
@@ -72,10 +78,10 @@ Route generate(vector< vector<double> > aWE, int e, int n, Route pR, vector<doub
 		{
 			break;
 		}
-		
+
 		currentCityEdges = 0;
 		ccIndex = 0;
-		
+
 		//get edges for current city here
 		for(int i = 0; i<aWE.size(); i++)
 		{
@@ -86,25 +92,25 @@ Route generate(vector< vector<double> > aWE, int e, int n, Route pR, vector<doub
 				currentCityEdges++;
 			}
 		}
-		
+
 		//one of the edges of the current city is randomly chosen here
 		currentEdge = fmod(rand(), currentCityEdges) + ccIndex;
-		
+
 		//the newest city is added to the nodeList
 		nList.push_back(aWE[currentEdge][1]);
-		
+
 		//add weight of edge to totalWeight
 		totalWeight += aWE[currentEdge][2];
-		
+
 		//set currentCity to the city to which we are travelling
 		currentCity = aWE[currentEdge][1];
-		
-		
+
+
 		noVisited = citiesVisited.size();
-		
+
 		/*
 		loop checks if the new currentCity is in the citiesVisited
-		vector. If it isn't, it's added to the vector and noVisited 
+		vector. If it isn't, it's added to the vector and noVisited
 		is incremented by one
 		*/
 		//consider replacing with std::find
@@ -121,16 +127,16 @@ Route generate(vector< vector<double> > aWE, int e, int n, Route pR, vector<doub
 				//noVisited++;
 			}
 		}
-		
+
 		cout<<"City: "<<currentCity<<" Cities Visited: "<<noVisited<<endl;
 	}
-	
+
 	//nodeList and total weight of route are added to the Route object
 	outputRoute.nodeList.swap(nList);
 	outputRoute.weight = totalWeight;
-	
+
 	return outputRoute;
-	
+
 }
 
 //function to complete a partial route
@@ -146,34 +152,34 @@ int main()
 {
 	//generate random seed
 	srand(time(0));
-	
+
 	//constant for # of edges in file
 	const int EDGES = 676;
 	//constant for # of nodes in file
 	const int NODES = 269;
-	
+
 	//open file
 	fstream inputFile;
 	inputFile.open ("network-small.txt", std::ios::in);
-	
+
 	/*
 	variables u, v, and w used for file input
 	totalWeight is used to get the total weight of all the edges
 	*/
 	double u, v, w, totalWeight = 0;
-	
+
 	//vector that stores edges from file
 	vector< vector<double> > allWeightedEdges;
-	
+
 	vector<double> nCount;
 	vector<double>::iterator it;
-	
+
 	//loop transfers file data to vector
 	while(inputFile >> u >> v >> w)
 	{
 		allWeightedEdges.push_back( {u,v,w} );
 		totalWeight += w;
-		
+
 		it = find(nCount.begin(), nCount.end(), u);
 		if(it == nCount.end())
 		{
@@ -185,17 +191,15 @@ int main()
 			nCount.push_back(v);
 		}
 	}
-	
+
 	cout << "Total weight of edges: " << totalWeight << endl;
-	
+
 	cout << "Total nodes: " << nCount.size() << endl;
-	
+
 	//route object generated here
-	vector<double> emptyVector;
-	Route emptyRoute;
-	Route r1 = generate(allWeightedEdges, EDGES, NODES, emptyRoute, emptyVector);
-	
+	Route r1 = generate(allWeightedEdges, EDGES, NODES);
+
 	cout<< "Route weight: " << r1.weight << endl;
-	
+
 	return 0;
 }
